9e3ff6df57522a35410ca9df09c3e08311d226b3
banderson1618@gmail.com <banderson1618@gmail.com>
2018-03-23 14:19:24 -0600

    Separated styles out in its own file

--- src/components/App.js
+++ src/components/App.js
@@ -12,6 +12,8 @@ import {
   View
 } from 'react-native';
 
+import styles from '../styles/Styles'
+
 export default class App extends Component {
   render() {
     return (
@@ -26,22 +28,3 @@ export default class App extends Component {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
