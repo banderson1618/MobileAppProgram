bb4fb44d83809ca33d3104877074291c5e2afd64
Intrepid <7chrispy7@gmail.com>
2018-03-28 13:58:16 -0600

    basic score display prototype

---
+++ src/components/ScoreScreen.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import {
+  Flatlist,
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import styles from '../styles/Styles'
+
+export default class ScoreScreen extends Component{
+  render(){
+    return()
+  }
+}
